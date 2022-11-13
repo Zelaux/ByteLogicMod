eb8ffdee6af22595ec8be0f97740f613fce5407c
Zelaux <ura20060605@gmail.com>
2022-11-13 17:20:06 +0300

    Fixed cursed sprites for "and", "or" and "xor" gates#3

--- core/src/bytelogic/world/blocks/logic/DisplayBlock.java
+++ core/src/bytelogic/world/blocks/logic/DisplayBlock.java
@@ -2,22 +2,24 @@ package bytelogic.world.blocks.logic;
 
 import arc.*;
 import arc.graphics.g2d.*;
+import mindustry.graphics.Pal;
 
-public class DisplayBlock extends AcceptorLogicBlock{
+public class DisplayBlock extends AcceptorLogicBlock {
 
-    public DisplayBlock(String name){
+    public DisplayBlock(String name) {
         super(name);
         rotate = false;
     }
 
     @Override
-    public TextureRegion[] icons(){
+    public TextureRegion[] icons() {
         return new TextureRegion[]{Core.atlas.find(name)};
     }
 
-    public class DisplayBuild extends AcceptorLogicBuild{
+    public class DisplayBuild extends AcceptorLogicBuild {
+
         @Override
-        public void draw(){
+        public void draw() {
             super.draw();
             //TODO buffer
             Draw.rect(region, tile.drawx(), tile.drawy());
@@ -25,12 +27,20 @@ public class DisplayBlock extends AcceptorLogicBlock{
             float dw = 2, dh = 2, xs = 2f, ys = 2f;
 
             int w = 5, h = 5;
-            for(int i = 0; i < w * h; i++){
+            int signal = currentSignal();
+            if (signal == 0) return;
+            if (signal < 0) Draw.color(Pal.remove);
+            signal &= ~1073741824;
+            if (signal < 0) signal = -signal;
+//            int jjj = 2 * ;
+
+
+            for (int i = 0; i < w * h; i++) {
                 int x = i % w;
                 int y = i / w;
 
-                if((currentSignal() & (1 << i)) != 0){
-                    Fill.rect(tile.drawx() + x*xs - (w-1) * xs/2f, tile.drawy() + y*ys - (h-1) * ys/2f, dw, dh);
+                if ((signal & (1 << i)) != 0) {
+                    Fill.rect(tile.drawx() + x * xs - (w - 1) * xs / 2f, tile.drawy() + y * ys - (h - 1) * ys / 2f, dw, dh);
                 }
             }
         }
